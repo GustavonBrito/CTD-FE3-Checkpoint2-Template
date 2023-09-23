b766a058179185b2e5e6015d48536e15d3fe1e78
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-09-22 23:03:15 -0300

    Consumindo dados da API para buscar nome e sobrenome do dentista na página Home

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,49 @@
+import React, { useEffect, useState } from "react";
 import styles from "./Card.module.css";
 
 const Card = () => {
+  const [dentistData, setDentistData] = useState(null);
+
+  useEffect(() => {
+    // Fazer a chamada à API para obter os dados do dentista
+    fetch("https://dhodonto.ctd.academy/dentista")
+      .then((response) => response.json())
+      .then((data) => {
+        // Assumindo que a API retorna um array de objetos, pegamos o primeiro item
+        // e atribuímos os campos "nome" e "sobrenome" às variáveis
+        if (data.length > 0) {
+          const primeiroDentista = data[0];
+          setDentistData({
+            nome: primeiroDentista.nome,
+            sobrenome: primeiroDentista.sobrenome,
+            matricula: primeiroDentista.matricula,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar dados do dentista:", error);
+      });
+  }, []);
 
   return (
     <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
-      <div className={`card`}>
-        <img
-          className="card-img-top"
-          src="/images/doctor.jpg"
-          alt="doctor placeholder"
-        />
-        <div className={`card-body ${styles.CardBody}`}>
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
-          que vem da API */}
-          <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
-          </a>
+      {dentistData && (
+        <div className={`card`}>
+          <img
+            className="card-img-top"
+            src="/images/doctor.jpg"
+            alt="doctor placeholder"
+          />
+          <div className={`card-body ${styles.CardBody}`}>
+            {/* Usar os dados do dentista para criar o link e mostrar o nome e sobrenome */}
+            <a href={`/dentist/${dentistData.matricula}`}>
+              <h5 className={`card-title ${styles.title}`}>
+                {dentistData.nome} {dentistData.sobrenome}
+              </h5>
+            </a>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
